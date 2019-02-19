272ea6efe7a475db945b1064c67489c97c9b477f
Kasparas Kažemėkas <kasparas.kazemekas@stud.vgtu.lt>
2019-02-19 17:10:57 +0200

    Pradinis commit

---
+++ Assets/Editor/MeshGeneratorInterface.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using System;
+
+[CustomEditor(typeof(MeshGeneratorControler), true)]
+[System.Serializable]
+public class MeshGeneratorInterface : Editor
+{
+    MeshGeneratorControler meshGeneratorControler;
+
+    public override void OnInspectorGUI()
+    {
+        meshGeneratorControler = (MeshGeneratorControler)target;
+        base.OnInspectorGUI();
+
+        if (GUILayout.Button("Export", GUILayout.Width(100)))
+        {
+            if (!String.IsNullOrEmpty(meshGeneratorControler.path))
+            {
+                ObjExporter.DoExport(false, meshGeneratorControler.GetExportObj(), meshGeneratorControler.path);
+
+            }
+        }
+        if (GUILayout.Button("Generate Mesh", GUILayout.Width(100)))
+        {
+            meshGeneratorControler.GenerateMesh();
+        }
+    }
+}
---
+++ Assets/MeshGeneratorControler.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeshGeneratorControler : MonoBehaviour {
+
+    public string path;
+    public GameObject generatedObj;
+
+	public void GenerateMesh()
+    {
+        generatedObj = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+    }
+    public GameObject GetExportObj()
+    {
+        return generatedObj;
+    }
+
+}
